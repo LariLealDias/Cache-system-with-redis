802022c4622a06a176d25d9d1c4df145d9c746c3
Lari <larileal@hotmail.com.br>
2023-12-09 16:34:31 -0300

    implementing main

--- app/main.py
+++ app/main.py
@@ -1,16 +1,38 @@
-# from fastapi import FastAPI
-# from app.controllers.auth_controller import router
-# app = FastAPI()
-
-# app.include_router(router, prefix="/auth")
-
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=8000)
-
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+from sqlalchemy import create_engine, MetaData
+from models.schemas.post_schema import PostBase
+from models.model.post_model_db import post
+from databases import Database
+
+load_dotenv('.env')  
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+metadata = MetaData()
+engine = create_engine(DATABASE_URL)
+database = Database(DATABASE_URL)
 
 app = FastAPI()
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+@app.post("/create-post/")
+async def create_post(post_data: PostBase):
+    query = post.insert().values(title=post_data.title, text=post_data.text)
+    last_record_id = await database.execute(query)
+    return {**post_data.dict(), "id": last_record_id}
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.on_event("startup")
+def create_tables():
+    metadata.create_all(engine)
