c30dd0fbae993cdc1e8ac5def2ccc263ad5f3aac
Lari <larileal@hotmail.com.br>
2023-12-11 20:21:54 -0300

    feat: creating POST router with redis

--- app/controllers/post_controller.py
+++ app/controllers/post_controller.py
@@ -4,8 +4,13 @@ from models.model.post_model_db import post
 import os
 from dotenv import load_dotenv
 from database import database
+import redis
+import json
+
 
 load_dotenv('.env')  
+
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
 router = APIRouter()
 
@@ -14,11 +19,13 @@ async def create_post(post_data: PostBase):
     try:
         query = post.insert().values(title=post_data.title, text=post_data.text)
         last_record_id = await database.execute(query)
+
+        resultadoRedis = redis_client.set(f"post_id: {last_record_id}", json.dumps(post_data.dict()))
+        print(resultadoRedis)
+
         return {**post_data.dict(), "id": last_record_id}
     except Exception as e:
         print(e)
         raise HTTPException(status_code=500, detail="Algo deu errado")
 
-@router.get("/")
-def read_root():
-    return {"Hello": "World"}
+
