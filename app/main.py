c482287db548bed92c1369a47241e14ab9dad728
Lari <larileal@hotmail.com.br>
2023-12-11 19:34:54 -0300

    refactor: refactoring main to accept routes

--- app/main.py
+++ app/main.py
@@ -1,37 +1,17 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
-import os
-from sqlalchemy import create_engine, MetaData
-from models.schemas.post_schema import PostBase
-from models.model.post_model_db import post
-from databases import Database
-
+from controllers.post_controller import router
+from database import database, metadata, engine
 load_dotenv('.env')  
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-metadata = MetaData()
-engine = create_engine(DATABASE_URL)
-database = Database(DATABASE_URL)
-
 app = FastAPI()
+
+app.include_router(router, prefix="/CRUD")
 
 @app.on_event("startup")
 async def startup():
     await database.connect()
-
-@app.on_event("shutdown")
-async def shutdown():
-    await database.disconnect()
-
-@app.post("/create-post/")
-async def create_post(post_data: PostBase):
-    query = post.insert().values(title=post_data.title, text=post_data.text)
-    last_record_id = await database.execute(query)
-    return {**post_data.dict(), "id": last_record_id}
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+    metadata.create_all(engine)
 
 @app.on_event("startup")
 def create_tables():
