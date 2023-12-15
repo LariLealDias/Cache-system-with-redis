e6d520fd70e4a0229273294649f2bcc03492936a
Lari <larileal@hotmail.com.br>
2023-12-15 09:04:16 -0300

    feat: new BaseModel to PATCH method

--- app/models/schemas/post_schema.py
+++ app/models/schemas/post_schema.py
@@ -1,9 +1,14 @@
+from typing import Optional
 from pydantic import BaseModel
 
 
 class PostBase(BaseModel):
     title: str
     text: str
+
+class PostBaseToUpdate(BaseModel):
+    title: Optional[str] = None
+    text:  Optional[str] = None
 
 class UserInDB(PostBase):
     id: int
